15a9e602a8037fb53061de47d7ca23df21cc5995
Darrell Mayson <mcdoob@gmail.com>
2019-12-26 10:54:44 +1300

    comments still broken - abandining branch

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :commentable, polymorphic: true
   belongs_to :parent, optional: true, class_name: "Comment"
 
